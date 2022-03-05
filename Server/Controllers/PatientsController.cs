8d88ee66f6a89a32ef62b20f912bf9636bf793aa
HMZ <hamzi1995@gmail.com>
2022-03-05 17:21:57 +0200

    Update action names and return validation errors as a ModelValidationResult

--- Server/Controllers/PatientsController.cs
+++ Server/Controllers/PatientsController.cs
@@ -24,7 +24,7 @@ namespace ClinicProject.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPatients(ODataQueryOptions<PatientDTO> options)
+        public async Task<IActionResult> Get(ODataQueryOptions<PatientDTO> options)
         {
             var query = await _context.Patients.GetQueryAsync<PatientDTO, Patient>(mapper, options);
 
@@ -32,7 +32,7 @@ namespace ClinicProject.Server.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPatientDTO(int key, ODataQueryOptions<PatientDTO> options)
+        public async Task<IActionResult> Get(int key, ODataQueryOptions<PatientDTO> options)
         {
             var result = await _context.Patients.Where(p => p.Id == key).GetQueryAsync(mapper, options);
 
@@ -41,7 +41,7 @@ namespace ClinicProject.Server.Controllers
 
         [HttpPut]
         [EnableQuery]
-        public async Task<IActionResult> PutPatientDTO(int key, [FromBody] PatientDTO patientdto)
+        public async Task<IActionResult> Put(int key, [FromBody] PatientDTO patientdto)
         {
             if (key != patientdto.Id)
                 return BadRequest();
@@ -81,20 +81,33 @@ namespace ClinicProject.Server.Controllers
 
         [HttpPost]
         [EnableQuery]
-        public async Task<ActionResult<Patient>> PostPatientDTO([FromBody] PatientDTO patientDTO)
+        public async Task<IActionResult> Post([FromBody] PatientDTO patientDTO)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+            {
+                var ValidationErrors = new ModelValidationResult
+                {
+                    Results = ModelState.ToDictionary(
+                        m => m.Key,
+                        m => string.Join('\n', m.Value.Errors.Select(e => e.ErrorMessage)))
+                };
 
-            await _context.Patients.AddAsync(mapper.Map<Patient>(patientDTO));
+                return BadRequest(ValidationErrors);
+            }
+
+            var patient = mapper.Map<Patient>(patientDTO);
+
+            await _context.Patients.AddAsync(patient);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetPatientDTO), new { id = patientDTO.Id }, patientDTO);
+            patientDTO.Id = patient.Id;
+
+            return CreatedAtAction(nameof(Get), new { key = patientDTO.Id }, patientDTO);
         }
 
         [HttpDelete]
         [EnableQuery]
-        public async Task<IActionResult> DeletePatientDTO(int key)
+        public async Task<IActionResult> Delete(int key)
         {
             var patient = await _context.Patients.FindAsync(key);
             if (patient == null)
