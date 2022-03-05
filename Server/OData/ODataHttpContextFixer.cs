637024e5fa81fb41f04c1ad9787e2a31a455747b
HMZ <hamzi1995@gmail.com>
2022-03-05 17:40:16 +0200

    HttpContext fixer middleware for odata

---
+++ Server/OData/ODataHttpContextFixer.cs
@@ -0,0 +1,19 @@
+﻿namespace ClinicProject.Server.OData
+{
+    public class ODataHttpContextFixer
+    {
+        private readonly RequestDelegate requestDelegate;
+
+        public ODataHttpContextFixer(RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IHttpContextAccessor contextAccessor)
+        {
+            contextAccessor.HttpContext ??= httpContext;
+
+            await requestDelegate(httpContext);
+        }
+    }
+}
