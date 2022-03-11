685e4f3522e828ab3f98164ec5b3b1fc6ce51d28
HMZ <hamzi1995@gmail.com>
2022-03-11 15:59:42 +0200

    Implement an AddPatient dialog to avoid dealing with FluentValidation and generics (not worth it)

---
+++ Client/Shared/Dialogs/Patients/AddPatient.cs
@@ -0,0 +1,38 @@
+﻿using ClinicProject.Shared.DTOs;
+using FluentValidation;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ClinicProject.Client.Shared.Dialogs.Patients
+{
+    public partial class AddPatient
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        [Inject] IValidator<PatientDTO> ModelValidator { get; set; }
+
+        MudForm Form;
+
+        public PatientDTO Model { get; set; } = new();
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ModelValidator.ValidateAsync(ValidationContext<PatientDTO>
+                .CreateWithOptions((PatientDTO)model, x => x.IncludeProperties(propertyName)));
+
+            if (result.IsValid)
+                return Array.Empty<string>();
+
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+
+        void Submit()
+        {
+            if (Form.IsValid)
+            {
+                MudDialog.Close(DialogResult.Ok(Model));
+            }
+        }
+
+        void Cancel() => MudDialog.Cancel();
+    }
+}
