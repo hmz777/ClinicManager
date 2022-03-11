360ac270b1e5806cccca00c3cd39a055c73dd8eb
HMZ <hamzi1995@gmail.com>
2022-03-11 15:56:45 +0200

    Add date ranges to the crud model.

--- Client/Models/CRUD/CRUDModel.cs
+++ Client/Models/CRUD/CRUDModel.cs
@@ -7,7 +7,25 @@
         public SortDirection SortDirection { get; set; }
         public int Page { get; set; }
         public int PageSize { get; set; }
-        public DateTime? From { get; set; }
-        public DateTime? To { get; set; }
+        public DateTime? CreatedFrom { get; set; }
+        public DateTime? CreatedUntil { get; set; }
+        public DateTime? UpdatedFrom { get; set; }
+        public DateTime? UpdatedUntil { get; set; }
+
+        public Dictionary<string, object> EqFilters { get; set; }
+
+        public bool HasFilter()
+        {
+            return (EqFilters != null && EqFilters.Count > 0)
+                || CreatedFrom != default
+                || CreatedUntil != default
+                || UpdatedFrom != default
+                || UpdatedUntil != default;
+        }
+
+        public bool HasSearch()
+        {
+            return !string.IsNullOrWhiteSpace(SearchString);
+        }
     }
 }
