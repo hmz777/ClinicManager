ed2808907e6f174337dc5680b63edf2f3ebe1663
HMZ <hamzi1995@gmail.com>
2022-03-12 21:53:03 +0200

    Create an add Appointment dialog

---
+++ Client/Shared/Dialogs/Appointments/AddAppointment.razor.cs
@@ -0,0 +1,38 @@
+﻿using ClinicProject.Shared.DTOs;
+using FluentValidation;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace ClinicProject.Client.Shared.Dialogs.Appointments
+{
+    public partial class AddAppointment
+    {
+        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        [Inject] IValidator<AppointmentDTO> ModelValidator { get; set; }
+
+        MudForm Form;
+
+        public AppointmentDTO Model { get; set; } = new();
+
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ModelValidator.ValidateAsync(ValidationContext<AppointmentDTO>
+                .CreateWithOptions((AppointmentDTO)model, x => x.IncludeProperties(propertyName)));
+
+            if (result.IsValid)
+                return Array.Empty<string>();
+
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+
+        void Submit()
+        {
+            if (Form.IsValid)
+            {
+                MudDialog.Close(DialogResult.Ok(Model));
+            }
+        }
+
+        void Cancel() => MudDialog.Cancel();
+    }
+}
