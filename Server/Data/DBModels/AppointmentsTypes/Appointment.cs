54766fd2a1ec2dcfb448ad04165f18d5e9c8d180
HMZ <hamzi1995@gmail.com>
2022-03-12 21:52:12 +0200

    Add a patient navigation property to be expanded on demand

--- Server/Data/DBModels/AppointmentsTypes/Appointment.cs
+++ Server/Data/DBModels/AppointmentsTypes/Appointment.cs
@@ -1,4 +1,5 @@
 ﻿using ClinicProject.Server.Data.DBModels.ModelBaseTypes;
+using ClinicProject.Server.Data.DBModels.PatientTypes;
 using ClinicProject.Shared.Models.Appointment;
 
 namespace ClinicProject.Server.Data.DBModels.AppointmentsTypes
@@ -7,5 +8,6 @@ namespace ClinicProject.Server.Data.DBModels.AppointmentsTypes
     {
         public virtual AppointmentType AppointmentType { get; set; }
         public virtual DateTime Date { get; set; }
+        public virtual Patient Patient { get; set; }
     }
 }
