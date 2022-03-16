0487acc9843c775458b31720f57b1377c719ee57
HMZ <hamzi1995@gmail.com>
2022-03-16 21:12:21 +0200

    Add an extension to check if a Tuple has a null value.

---
+++ Client/Helpers/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ClinicProject.Client.Helpers
+{
+    public static class Extensions
+    {
+        public static bool HasNull(this ITuple tuple)
+        {
+            for (int i = 0; i < tuple.Length; i++)
+            {
+                if (tuple[i] == null)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
