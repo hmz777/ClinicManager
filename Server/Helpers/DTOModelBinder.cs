46fbf114684bb14c0814878ffd4509fd009cb4eb
HMZ <hamzi1995@gmail.com>
2022-03-26 16:56:34 +0300

    Refactorings

--- Server/Helpers/DTOModelBinder.cs
+++ Server/Helpers/DTOModelBinder.cs
@@ -13,6 +13,7 @@ namespace ClinicProject.Server.Helpers
         {
             this.jsonOptions = jsonOptions;
         }
+
         public async Task BindModelAsync(ModelBindingContext bindingContext)
         {
             if (bindingContext == null)
@@ -39,6 +40,60 @@ namespace ClinicProject.Server.Helpers
                 }
 
                 var model = JsonSerializer.Deserialize<T>(bodyJson, jsonOptions.Value.JsonSerializerOptions);
+
+                if (model == null)
+                {
+                    return;
+                }
+
+                bindingContext.Result = ModelBindingResult.Success(model);
+
+                return;
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+
+    public class DTOModelBinder : IModelBinder
+    {
+        private readonly Type type;
+        private readonly IOptions<JsonOptions> jsonOptions;
+
+        public DTOModelBinder(Type type, IOptions<JsonOptions> jsonOptions)
+        {
+            this.type = type;
+            this.jsonOptions = jsonOptions;
+        }
+
+        public async Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            try
+            {
+                bindingContext.HttpContext.Request.EnableBuffering();
+
+                var bodyStream = bindingContext.HttpContext.Request.Body;
+
+                string bodyJson;
+
+                using (StreamReader sr = new(bodyStream))
+                {
+                    bodyJson = await sr.ReadToEndAsync();
+                }
+
+                if (string.IsNullOrWhiteSpace(bodyJson))
+                {
+                    return;
+                }
+
+                var model = JsonSerializer.Deserialize(bodyJson, type, jsonOptions.Value.JsonSerializerOptions);
 
                 if (model == null)
                 {
